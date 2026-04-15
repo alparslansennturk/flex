474beaba85ed79f69370710d8c96dc953d04d980
Alparslan Şentürk <alparslansennturk@gmail.com>
2026-04-15 13:11:24 +0300

    altyapı

---
+++ src/app/dashboard/test/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="flex items-center justify-center h-64">
+      <p className="text-[18px] font-bold text-surface-400">Test Alanı</p>
+    </div>
+  );
+}
